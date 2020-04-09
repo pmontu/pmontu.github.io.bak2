02dd539ee74894c63ec619f8679d8f1626090896
pmontu <pmontu@gmail.com>
2020-04-09 22:23:25 +0530

    react-stock-market-hacker-news-dashboard

---
+++ react-stock-market-hacker-news-dashboard/precache-manifest.80f7f0aa10bcf33c35e9cfafe7353403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6faadb7774191b9536f93d56aed73d",
+    "url": "/react-stock-market-hacker-news-dashboard/index.html"
+  },
+  {
+    "revision": "3ac03d0524bcc6c13088",
+    "url": "/react-stock-market-hacker-news-dashboard/static/css/main.37242060.chunk.css"
+  },
+  {
+    "revision": "dc54b1f215ca4c6d0036",
+    "url": "/react-stock-market-hacker-news-dashboard/static/js/2.ac8f1073.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-stock-market-hacker-news-dashboard/static/js/2.ac8f1073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac03d0524bcc6c13088",
+    "url": "/react-stock-market-hacker-news-dashboard/static/js/main.71389a96.chunk.js"
+  },
+  {
+    "revision": "52dfbe3e233c8dc31f5e",
+    "url": "/react-stock-market-hacker-news-dashboard/static/js/runtime-main.de5fb7f7.js"
+  }
+]);
